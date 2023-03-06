954ca7393ebedf3871f20114b7696a836eef4058
박준영 <duqrlqkrwnl@gmail.com>
2023-03-06 11:18:36 +0900

    #9 insert 기본키 테스트중

--- src/main/java/com/hotsix/titans/attendanceManagement/entity/LeaveCategory.java
+++ src/main/java/com/hotsix/titans/attendanceManagement/entity/LeaveCategory.java
@@ -1,11 +1,10 @@
 package com.hotsix.titans.attendanceManagement.entity;
 
 import lombok.*;
+import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.Parameter;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @AllArgsConstructor
 @NoArgsConstructor
@@ -14,10 +13,22 @@ import javax.persistence.Table;
 @ToString
 @Entity
 @Table(name = "TBL_LEAVE_CATEGORY")
+@SequenceGenerator(
+        name = "LEAVE_CATEGORY_SEQ_GENERATOR",
+        sequenceName = "SEQ_LEAVE_CATEGORY_CODE",
+        initialValue = 1, allocationSize = 1
+)
 public class LeaveCategory {
+
 
     @Id
     @Column(name = "LEAVE_CATEGORY_CODE")
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "leave_category_seq")
+    @GenericGenerator(name = "leave_category_seq", strategy = "com.hotsix.titans.commons.StringPrefixSequenceGenerator",
+            parameters = {
+                    @Parameter(name = "sequence_name", value = "SEQ_LEAVE_CATEGORY_CODE"),
+                    @Parameter(name = "prefix", value = "LC")
+            })
     private String leaveCategoryCode;       // 휴가구분번호
 
     @Column(name = "LEAVE_CATEGORY_NAME")
