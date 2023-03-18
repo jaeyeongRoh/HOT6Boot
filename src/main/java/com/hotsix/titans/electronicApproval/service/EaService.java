de00325ba6e0271957d57bfa54cd249a2cf6cbc7
jyR <kfmxyqtycn1@gmail.com>
2023-03-18 15:51:51 +0900

    fix: 전자결재 결재자 로직 수정 #79

--- src/main/java/com/hotsix/titans/electronicApproval/service/EaService.java
+++ src/main/java/com/hotsix/titans/electronicApproval/service/EaService.java
@@ -199,7 +199,6 @@ public class EaService {
      */
     @Transactional
     public Object insertLeave(EaLeaveDTO eaLeaveDTO) {
-        EaLeave eaLeave;
 
         eaLeaveDTO.setEaDate(LocalDate.now());
         eaLeaveDTO.setLeaveCategoryCode("LC1");
@@ -207,6 +206,8 @@ public class EaService {
         eaLeaveDTO.setLeaveStartDate(LocalDate.now());
         eaLeaveDTO.setLeaveEndDate(LocalDate.now());
         log.info("getEaApproverInfoListDTO{}", eaLeaveDTO.getEaApproverInfoList());
+
+        EaLeave eaLeave;
         eaLeave = modelMapper.map(eaLeaveDTO, EaLeave.class);
 
         eaLeave.setEaApproverInfoList(eaLeaveDTO.getEaApproverInfoList().stream().map(eaApproverInfoDTO -> modelMapper.map(eaApproverInfoDTO, EaApproverInfo.class)).collect(Collectors.toList()));
